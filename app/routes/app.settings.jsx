775118ce96cebe0e2fd7f7b75489f307cb283dc1
Aditi Patil <patiladiti240@gmail.com>
2024-03-21 11:31:07 +0530

    Database structures and Prisma in Remix

--- app/routes/app.settings.jsx
+++ app/routes/app.settings.jsx
@@ -14,19 +14,46 @@ import { useState } from "react";
 import { json } from "@remix-run/node"
 import { useLoaderData, Form } from "@remix-run/react";
 
+// Import prisma db
+import db from "../db.server.js"
+
 export async function loader() {
-    // get data from database
+    /*
+    // static data
     let settings = {
         name: "My app",
         description: "My app description"
     }
 
     return json(settings)
+    */
+
+    // get data from database
+    let settings = await db.settings.findFirst();
+    return json(settings)
 }
 
 export async function action({ request }) {
     let settings = await request.formData();
     settings = Object.fromEntries(settings)
+
+    // update the database
+    await db.settings.upsert({
+        where: {
+            id: '1'
+        },
+        update: {
+            id: '1',
+            name: settings.name,
+            description: settings.description
+        },
+        create: {
+            id: '1',
+            name: settings.name,
+            description: settings.description
+        }
+    })
+
     return json({ settings: settings, message: 'Settings updated' })
 }
 
@@ -59,8 +86,8 @@ export default function SettingsPage() {
                     <Card roundedAbove="sm">
                         <Form method="POST">
                             <BlockStack gap="400">
-                                <TextField label="App name" name="name" value={formSaved.name} onChange={(value) => setFormSaved({ ...formSaved, name: value })} />
-                                <TextField label="Description" name="description" value={formSaved.description} onChange={(value) => setFormSaved({ ...formSaved, description: value })} />
+                                <TextField label="App name" name="name" value={formSaved?.name} onChange={(value) => setFormSaved({ ...formSaved, name: value })} />
+                                <TextField label="Description" name="description" value={formSaved?.description} onChange={(value) => setFormSaved({ ...formSaved, description: value })} />
                                 <Button submit={true}>Save</Button>
                             </BlockStack>
                         </Form>
