db2051e3efe624495b6e1f4133a5840ba48bdb63
Aditi Patil <patiladiti240@gmail.com>
2024-03-22 16:16:48 +0530

    Wishlist table created, Wishlist post api created, Saved wishlist data in db

--- app/routes/api.wishlist.jsx
+++ app/routes/api.wishlist.jsx
@@ -1,4 +1,6 @@
 import { json } from "@remix-run/node"
+import db from "../db.server"
+import { cors } from "remix-utils/cors"
 
 export async function loader() {
     return json({
@@ -9,13 +11,37 @@ export async function loader() {
 
 export async function action({ request }) {
     const method = request.method
+
+    let data = await request.formData()
+    data = Object.fromEntries(data)
+    const customerId = data.customerId
+    const productId = data.productId
+    const shop = data.shop
+
+    if (!customerId || !productId || !shop) {
+        return json({
+            message: "Missing data. Required data: customerId, productId, shop",
+            method: method
+        })
+    }
+
     switch (method) {
         case "POST":
-            return json({ message: "Success", method: "POST" });
-            break;
+            const wishlist = await db.wishlist.create({
+                data: {
+                    customerId,
+                    productId,
+                    shop
+                }
+            })
+
+            const response = json({ message: "Product added to wishlist", method: "POST", wishlist: wishlist })
+            return cors(request, response);
         case "PATCH":
             return json({ message: "Success", method: "PATCH" });
             break;
+        case "DELETE":
+            return json({ message: "Product removed from your wishlist", method: "DELETE" })
         default:
             return new Response("Method Not Allowed", { status: 405 })
     }
